fixed vanishing gradients with mirrored leaky relu

multipleFeatureLearnerTrees/CDT_fl.py
@@ -19,7 +19,10 @@ class CDT_fl(nn.Module):
         self.device = torch.device(dc_args.get('device', 'cpu'))
         self.name = name
 
-        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+        self.LeakyReLU = nn.LeakyReLU(
+            negative_slope=self.args.get("tree_LReLU_neg_slope", 0.05)
+        )
 
         self.feature_learning_init()
 
@@ -101,12 +104,13 @@ class CDT_fl(nn.Module):
         self.batch_size = batch_size
         aug_data = self._data_augment_(data)
         # (batch_size, num_fl_inner_nodes)
-        path_prob = self.sigmoid(
-            self.beta_fl * self.fl_inner_nodes(aug_data)
-        )
+        path_prob = self.beta_fl * self.fl_inner_nodes(aug_data)
 
         path_prob = torch.unsqueeze(path_prob, dim=2)
-        path_prob = torch.cat((path_prob, 1-path_prob), dim=2)
+        path_prob = torch.cat(
+            (self.LeakyReLU(path_prob), self.LeakyReLU(-path_prob)),
+            dim=2
+        )
         _mu = aug_data.data.new(self.batch_size, 1, 1).fill_(1.)
 
         begin_idx = 0
@@ -115,11 +119,11 @@ class CDT_fl(nn.Module):
             _path_prob = path_prob[:, begin_idx:end_idx, :]
 
             _mu = _mu.view(self.batch_size, -1, 1).repeat(1, 1, 2)
-            _mu = _mu * _path_prob
+            _mu = _mu + _path_prob
             begin_idx = end_idx  # index for each layer
             end_idx = begin_idx + 2 ** (layer_idx + 1)
         mu = _mu.view(self.batch_size, self.num_fl_leaves)
-        return mu
+        return self.softmax(mu)
 
     def intermediate_features_construct(self, data):
         """

multipleFeatureLearnerTrees/CDT_hierarchical_fl.py
@@ -14,34 +14,38 @@ import numpy as np
 
 """
 learner_args = {
-    "feature_learner_args"          : [
+    "feature_learner_args"          : [ # can have as many of these as needed
         {
             "num_intermediate_variables"    : 8,
             "feature_learning_depth"        : 2,
+            "tree_LReLU_neg_slope"          : 0.05,
             "weight_decay"                  : 0.001,
             "exp_scheduler_gamma"           : 1.,
             "beta_fl"                       : 1,
         },
         {
-            "num_intermediate_variables"    : 8,
-            "feature_learning_depth"        : 2,
+            "num_intermediate_variables"    : 4,
+            "feature_learning_depth"        : 3,
+            "tree_LReLU_neg_slope"          : 0.05,
             "weight_decay"                  : 0.001,
             "exp_scheduler_gamma"           : 1.,
             "beta_fl"                       : 1,
         }
-    ]
+    ],
     "decision_depth"                : 4,
-    "lr"                            : 1e-3,
-    "K_epoch"                       : 1,
+    "tree_LReLU_neg_slope"          : 0.05,
+    "lr"                            : 2e-3,
+    "K_epoch"                       : 15,
     "weight_decay"                  : 0.001,
-    "gamma"                         : 0.0,
+    "gamma"                         : 0.2,
     "exp_scheduler_gamma"           : 1.,
-    "device"                        : "cuda:0" if torch.cuda.is_available() else "cpu",
+    "device"                        : "cpu", # "cuda:0" if torch.cuda.is_available() else "cpu",
     "greatest_path_probability"     : 0,
     "beta_dc"                       : 1,
     "classification"                : 1,
     "learn_probabilities"           : 1,
     "fixed_model_weight"            : 1,
+    "loss_batch_size"               : 5,
 }
 """
 
@@ -53,8 +57,10 @@ class CDT(nn.Module):
         # print('CDT parameters: ', args)
         self.device = torch.device(self.args.get('device', 'cpu'))
 
-        self.sigmoid = nn.Sigmoid()
         self.softmax = nn.Softmax(dim=1)
+        self.LeakyReLU = nn.LeakyReLU(
+            negative_slope=self.args.get("tree_LReLU_neg_slope", 0.05)
+        )
 
         self.feature_learning_init(self.args['feature_learner_args'])
         self.decision_init()
@@ -213,15 +219,17 @@ class CDT(nn.Module):
 
         aug_features = self._data_augment_(self.features)
         # (batch_size * num_fl_leaves, num_dc_inner_nodes)
-        path_prob = self.sigmoid(
-            self.beta_dc * self.dc_inner_nodes(aug_features)
-        )
+        path_prob = self.beta_dc * self.dc_inner_nodes(aug_features)
+
         # batch_size * num_fl_leaves
         feature_batch_size = self.features.shape[0]
 
         path_prob = torch.unsqueeze(path_prob, dim=2)
         # (batch_size * num_fl_leaves, num_dc_inner_nodes, 2)
-        path_prob = torch.cat((path_prob, 1 - path_prob), dim=2)
+        path_prob = torch.cat(
+            (self.LeakyReLU(path_prob), self.LeakyReLU(-path_prob)),
+            dim=2
+        )
         # (batch_size * num_fl_leaves, 1, 1)
         _mu = aug_features.data.new(feature_batch_size, 1, 1).fill_(1.)
 
@@ -231,12 +239,12 @@ class CDT(nn.Module):
             _path_prob = path_prob[:, begin_idx:end_idx, :]
 
             _mu = _mu.view(feature_batch_size, -1, 1).repeat(1, 1, 2)
-            _mu = _mu * _path_prob
+            _mu = _mu + _path_prob
             begin_idx = end_idx  # index for each layer
             end_idx = begin_idx + 2 ** (layer_idx + 1)
         # (batch_size * num_fl_leaves, num_dc_inner_nodes)
         mu = _mu.view(feature_batch_size, self.num_dc_leaves)
-        return mu
+        return self.softmax(mu)
 
     def decision_leaves(self, p):
         if self.args["classification"]:

multipleFeatureLearnerTrees/train_boosted_CDT.py
@@ -112,6 +112,11 @@ class PPO(nn.Module):
         if not self.learn_probabilities:
             loss = self.softXEnt(q_vals, max_target_mask)
         else:
+            # https://www.desmos.com/calculator/syrhadqppv
+            offset = ((td_target > 0.5) * (-0.01)) + ((td_target <= 0.5) * 0.01)
+            q_vals += offset
+            q_vals[q_vals > 1] = 1
+            q_vals[q_vals < 0] = 0
             loss = self.softXEnt(q_vals, td_target)
 
         # orient loss so minimum is 0, only effects learn_probabilities
@@ -189,7 +194,7 @@ class BoostedPPO:
             ).to(self.learner_args['device'])
 
             # insert train data and train model
-            model.train = self.train
+            model.train = self.train.copy()
             model.train_net()
 
             # eval model on train for per sample loss
@@ -335,13 +340,16 @@ class BoostedPPO:
         )
         return average, np.sqrt(variance)
 
-    def get_feature_importances(self, use_estimator_weights=True):
+    def get_feature_importances(self, use_estimator_weights=True, binary=False):
         feature_weights = []
         for estimator in self.estimators:
             tree_weights = estimator['estimator'].cdt.get_tree_weights()
             fl_weights = estimator['estimator'].cdt.get_fl_input_weights()
 
             dc_tree_imp = np.mean(np.abs(tree_weights[1]), axis=0)
+            if binary:
+                dc_tree_imp = (dc_tree_imp > 0).astype(float)
+
             # get normalized importance of each feature in endmost inner nodes
             feat_tree_imp = dc_tree_imp[np.newaxis, :]
             feat_tree_imp /= feat_tree_imp.sum()
@@ -349,12 +357,16 @@ class BoostedPPO:
                 # get normalized importance of each feature in leaves
                 # preceeding the tree importance
                 fl_leaf_imp = np.mean(np.abs(fl_weights[i]), axis=0)
+                if binary:
+                    fl_leaf_imp = (fl_leaf_imp > 0).astype(float)
                 fl_leaf_imp /= fl_leaf_imp.sum()
                 # dot prod of importances for the tree onto the
                 # leaves preceeding it
                 fl_feat_imp = feat_tree_imp @ fl_leaf_imp
                 # get normalized importance of each feature in inner nodes prior
                 fl_tree_imp = np.mean(np.abs(tree_weights[0][i]), axis=0)
+                if binary:
+                    fl_tree_imp = (fl_tree_imp > 0).astype(float)
                 fl_tree_imp /= fl_tree_imp.sum()
                 # add normalized importance of features for sub-feature trees
                 # to normalized importance of features for branching logic leading

singleFeatureLearnerTree/CDT.py
@@ -15,8 +15,10 @@ class CDT(nn.Module):
         # print('CDT parameters: ', args)
         self.device = torch.device(self.args.get('device', None))
 
-        self.sigmoid = nn.Sigmoid()
         self.softmax = nn.Softmax(dim=1)
+        self.LeakyReLU = nn.LeakyReLU(
+            negative_slope=self.args.get("tree_LReLU_neg_slope", 0.05)
+        )
 
         self.feature_learning_init()
         self.decision_init()
@@ -101,12 +103,13 @@ class CDT(nn.Module):
         Return the probabilities for reaching each leaf.
         """
         # (batch_size, num_fl_inner_nodes)
-        path_prob = self.sigmoid(
-            self.beta_fl * self.fl_inner_nodes(self.aug_data)
-        )
+        path_prob = self.beta_fl * self.fl_inner_nodes(self.aug_data)
 
         path_prob = torch.unsqueeze(path_prob, dim=2)
-        path_prob = torch.cat((path_prob, 1-path_prob), dim=2)
+        path_prob = torch.cat(
+            (self.LeakyReLU(path_prob), self.LeakyReLU(-path_prob)),
+            dim=2
+        )
         _mu = self.aug_data.data.new(self.batch_size, 1, 1).fill_(1.)
 
         begin_idx = 0
@@ -115,11 +118,11 @@ class CDT(nn.Module):
             _path_prob = path_prob[:, begin_idx:end_idx, :]
 
             _mu = _mu.view(self.batch_size, -1, 1).repeat(1, 1, 2)
-            _mu = _mu * _path_prob
+            _mu = _mu + _path_prob
             begin_idx = end_idx  # index for each layer
             end_idx = begin_idx + 2 ** (layer_idx + 1)
         mu = _mu.view(self.batch_size, self.num_fl_leaves)
-        return mu
+        return self.softmax(mu)
 
     def decision_init(self):
         self.num_dc_inner_nodes = 2 ** self.args['decision_depth'] - 1
@@ -158,15 +161,16 @@ class CDT(nn.Module):
         # (batch_size * num_fl_leaves, num_intermediate_variables + 1)
         aug_features = self._data_augment_(self.features)
         # (batch_size * num_fl_leaves, num_dc_inner_nodes)
-        path_prob = self.sigmoid(
-            self.beta_dc * self.dc_inner_nodes(aug_features)
-        )
+        path_prob = self.beta_dc * self.dc_inner_nodes(aug_features)
         # batch_size * num_fl_leaves
         feature_batch_size = self.features.shape[0]
 
         path_prob = torch.unsqueeze(path_prob, dim=2)
         # (batch_size * num_fl_leaves, num_dc_inner_nodes, 2)
-        path_prob = torch.cat((path_prob, 1 - path_prob), dim=2)
+        path_prob = torch.cat(
+            (self.LeakyReLU(path_prob), self.LeakyReLU(-path_prob)),
+            dim=2
+        )
         _mu = aug_features.data.new(feature_batch_size, 1, 1).fill_(1.)
 
         begin_idx = 0
@@ -175,12 +179,11 @@ class CDT(nn.Module):
             _path_prob = path_prob[:, begin_idx:end_idx, :]
 
             _mu = _mu.view(feature_batch_size, -1, 1).repeat(1, 1, 2)
-            _mu = _mu * _path_prob
+            _mu = _mu + _path_prob
             begin_idx = end_idx  # index for each layer
             end_idx = begin_idx + 2 ** (layer_idx + 1)
         mu = _mu.view(feature_batch_size, self.num_dc_leaves)
-
-        return mu
+        return self.softmax(mu)
 
     def intermediate_features_construct(self):
         """

singleFeatureLearnerTree/GYM_single_stock_indicators.py
@@ -66,7 +66,7 @@ class DerivativesTradingEnv(gym.Env):
     def __init__(
         self, df, train_end_ratio=None, n_lookback=30,
         trade_length=5, derivative_type=TimedTrailingStop(),
-        ignore_indicators=[]
+        ignore_indicators=[], reward_weight=1
     ):
         super().__init__()
 
@@ -81,6 +81,7 @@ class DerivativesTradingEnv(gym.Env):
         self.n_lookback = n_lookback
         self.trade_length = trade_length # action space? (0, np.inf)
         self.derivative_type = derivative_type
+        self.reward_weight = reward_weight
 
         # Actions of the format [Q-Sell, Q-Do nothing (=0), Q-Buy]
         self.action_space = spaces.Box(
@@ -128,7 +129,7 @@ class DerivativesTradingEnv(gym.Env):
         return (self.derivative_type.make_order(
             lookahead / current_price,
             order_type=action_type
-        ) - 1) * 100
+        ) - 1) * self.reward_weight
 
     def step(self, action):
         # Execute one time step within the environment

singleFeatureLearnerTree/train_CDT.py
@@ -150,13 +150,20 @@ class PPO(nn.Module):
         # get prediction and target
         q_vals = self.calc_probs(s)
         td_target = self.softmax(rs) + self.gamma * self.calc_probs(s_prime)
-        td_target = td_target / torch.sum(td_target, dim=1)
-        max_target_mask = torch.eq(td_target, td_target.max(dim=1)[0]).float()
+        td_target = td_target / torch.sum(td_target, dim=1).unsqueeze(1)
+        max_target_mask = torch.eq(
+            td_target, td_target.max(dim=1)[0].unsqueeze(1)
+        ).float()
 
         # calc cross entropy loss using target mask or real probabilities
         if not self.learn_probabilities:
             loss = self.softXEnt(q_vals, max_target_mask)
         else:
+            # https://www.desmos.com/calculator/syrhadqppv
+            offset = ((td_target > 0.5) * (-0.01)) + ((td_target <= 0.5) * 0.01)
+            q_vals += offset
+            q_vals[q_vals > 1] = 1
+            q_vals[q_vals < 0] = 0
             loss = self.softXEnt(q_vals, td_target)
 
         # orient loss so minimum is 0, only effects learn_probabilities